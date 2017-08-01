2e158e264eacf5c91ac23ec8b90e74aeb8c62afa
gPlachno <gPlachno@UST-MIS-L-203>
2017-08-01 13:53:06 -0400

    ServerCards work + ping

--- ServerManager/Form1.Designer.cs
+++ ServerManager/Form1.Designer.cs
@@ -29,36 +29,188 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.overViewlabel = new System.Windows.Forms.Label();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 85);
+            this.button1.Location = new System.Drawing.Point(35, 156);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(75, 31);
             this.button1.TabIndex = 0;
             this.button1.Text = "Add Server";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(140, 50);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // overViewlabel
+            // 
+            this.overViewlabel.AutoSize = true;
+            this.overViewlabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(225)))), ((int)(((byte)(225)))));
+            this.overViewlabel.Font = new System.Drawing.Font("Roboto", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.overViewlabel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.overViewlabel.Location = new System.Drawing.Point(23, 58);
+            this.overViewlabel.Name = "overViewlabel";
+            this.overViewlabel.Size = new System.Drawing.Size(98, 25);
+            this.overViewlabel.TabIndex = 2;
+            this.overViewlabel.Text = "Overview";
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(426, 156);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(213, 172);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(426, 156);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(213, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Add a Server";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(437, 189);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Server Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(440, 205);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(187, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(440, 295);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Accept";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(552, 295);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Cancel";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(440, 256);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(187, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(437, 240);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Server IP";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(35, 203);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Suite
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(233)))), ((int)(((byte)(233)))));
             this.ClientSize = new System.Drawing.Size(984, 611);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.overViewlabel);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Suite";
             this.Text = "Server Manager";
             this.Load += new System.EventHandler(this.Suite_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        public System.Windows.Forms.Label overViewlabel;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- ServerManager/Form1.cs
+++ ServerManager/Form1.cs
@@ -1,26 +1,44 @@
 ﻿using System;
+using System.Collections;
 using System.Drawing;
-using System.Drawing.Text;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;   
 
 namespace ServerManager
 {
     public partial class Suite : Form
     {
+        //Control definitions
         private PictureBox headerArea = new PictureBox();
         private PictureBox dropShadow = new PictureBox();
+        private PictureBox dropShadow1 = new PictureBox();
+        private PictureBox sideArea = new PictureBox();
         private Label headerText = new Label();
-        private Panel serverArea = new Panel();
-        
+        private FlowLayoutPanel serverArea = new FlowLayoutPanel();
+        //private Panel tesgt = new Panel();
+       
+        public int picBox_ID = 0;
 
-        //test
+
+        //ArrayList testBruh = new ArrayList();
+
+        //Server Array List
+        public ArrayList serverIPs = new ArrayList();
+        public ArrayList serverNames = new ArrayList();
+
         //Variables
         public int startX = 5;
         public int startY = 5;
         public int cardSizeX = 200;
         public int cardSizeY = 150;
-
-        PaintEventArgs e;
+        public string serverName;
+        public string serverIP;
+        public string serverSpace;
+        public string serverMS;
+        public int serverCount = 0;
 
         public Suite()
         {
@@ -28,11 +46,110 @@ namespace ServerManager
 
         }
 
+        //Form loading
         private void Suite_Load(object sender, EventArgs e)
         {
+            //Load GUI
             guiInit();
+            
+            //Excel read
+            excelRead();
+
+            MessageBox.Show(serverIPs.Count.ToString());
+            //serverCard make
+            excelToCard(serverIPs.Count);
+
+            //making stuff invisible
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            textBox1.Visible = false;
+            textBox2.Visible = false;
+            button2.Visible = false;
+            button3.Visible = false;
+            pictureBox2.Visible = false;
+
+            //FORDEV
+            pictureBox1.Visible = false;
         }
-        
+
+        //making cards using excel
+        private void excelToCard(int num)
+        {
+            for(int i = 0; i < num; i++)
+            {
+                serverCard(1, i);
+
+            }
+        }
+
+        //reading excel
+        private void excelRead()
+        {
+            {
+
+                //Create COM Objects. Create a COM object for everything that is referenced
+                Excel.Application xlApp = new Excel.Application();
+                Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"S:\Utils\documents\ServerManager\serverlist.xlsx");
+                Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+                Excel.Range xlRange = xlWorksheet.UsedRange;
+
+                int rowCount = xlRange.Rows.Count;
+                int colCount = xlRange.Columns.Count;
+
+                //iterate over the rows and columns and print to the console as it appears in the file
+                //excel is not zero based!!
+                for (int i = 2; i <= rowCount; i++)
+                {
+                    for (int j = 1; j <= colCount; j++)
+                    {
+                        //new line
+                        if (j == 1)
+                        {
+                            //Console.Write("\r\n");
+                            if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
+                            {
+                                Console.Write(xlRange.Cells[i, j].Value2.ToString() + "\t");
+                                serverNames.Add(xlRange.Cells[i, j].Value2.ToString());
+                            }
+                            
+                        }
+
+                        if (j == 2)
+                        {
+                            //Console.Write("\r\n");
+                            if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
+                            {
+                                //Console.Write(xlRange.Cells[i, j].Value2.ToString() + "\t");
+                                serverIPs.Add(xlRange.Cells[i, j].Value2.ToString());
+                            }
+
+                        }
+                    }
+                }
+
+                //cleanup
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+
+                //rule of thumb for releasing com objects:
+                //  never use two dots, all COM objects must be referenced and released individually
+                //  ex: [somthing].[something].[something] is bad
+
+                //release com objects to fully kill excel process from running in the background
+                Marshal.ReleaseComObject(xlRange);
+                Marshal.ReleaseComObject(xlWorksheet);
+
+                //close and release
+                xlWorkbook.Close();
+                Marshal.ReleaseComObject(xlWorkbook);
+
+                //quit and release
+                xlApp.Quit();
+                Marshal.ReleaseComObject(xlApp);
+            }
+        }
+        //Sets up the default GUI for the program
         private void guiInit()
         {
             //text
@@ -58,43 +175,257 @@ namespace ServerManager
             this.dropShadow.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.dropShadow.Location = new System.Drawing.Point(0, 37);
             this.dropShadow.Name = "dropShadow";
-            this.dropShadow.Size = new System.Drawing.Size(1920, 20);
+            this.dropShadow.Size = new System.Drawing.Size(140, 20);
             this.dropShadow.TabIndex = 10;
             this.dropShadow.TabStop = false;
+            this.dropShadow.BackColor = Color.FromArgb(225, 225, 225);
             this.Controls.Add(dropShadow);
 
-            
+            //Drop Shadow
+            this.dropShadow1.Image = global::ServerManager.Properties.Resources.dropshadow2;
+            this.dropShadow1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.dropShadow1.Location = new System.Drawing.Point(140, 37);
+            this.dropShadow1.Name = "dropShadow1";
+            this.dropShadow1.Size = new System.Drawing.Size(1920, 20);
+            this.dropShadow1.TabIndex = 10;
+            this.dropShadow1.TabStop = false;
+            this.dropShadow1.BackColor = Color.FromArgb(233, 233, 233);
+            this.Controls.Add(dropShadow1);
+
             //server panel
-            this.serverArea.Size = new Size(843, this.Height - 55);
+            this.serverArea.Size = new Size(843, 550);
             this.serverArea.Location = new Point(140, 55);
             this.serverArea.AutoScroll = true;
             this.Controls.Add(serverArea);
 
-            
+            //sideArea
+            this.sideArea.Size = new Size(140, 1000);
+            this.sideArea.Location = new Point(0, 0);
+            this.sideArea.BackColor = Color.FromArgb(225, 225, 225);
+            this.Controls.Add(sideArea);
         }
 
+        //creats servers
         private void button1_Click(object sender, EventArgs e)
         {
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            textBox1.Visible = true;
+            textBox2.Visible = true;
+            button2.Visible = true;
+            button3.Visible = true;
+            pictureBox2.Visible = true;
+             
+        }
 
-            for (int i = 0; i < 1; i++)
+        //serverCard
+        public void serverCard(int num, int count)
+        {
+            //Vars
+            string ip = serverIPs[count].ToString();
+            string name = serverNames[count].ToString();
+
+            //Setting false
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            textBox1.Visible = false;
+            textBox2.Visible = false;
+            button2.Visible = false;
+            button3.Visible = false;
+            pictureBox2.Visible = false;
+            textBox1.Text = "";
+            textBox2.Text = "";
+
+            //Temp fix for the scrolling and server card pos issue
+            serverArea.AutoScrollPosition = new Point(0, 0);
+
+            //creates a server card, dont know why its in a loop maybe for excel 
+            for (int i = 0; i < num; i++)
             {
                 //Definitions
-                Button cardBack = new Button();
-                //Specifics
+                PictureBox cardBack = new PictureBox();
+                PictureBox pingLabel = new PictureBox();
+                Label serverNameLabel = new Label();
+                Label serverIPLabel = new Label();
+                Panel tesgt = new Panel();
+
+                //Panel
+                tesgt.Size = new Size(cardSizeX, cardSizeY);
+                tesgt.Top = startY;
+                tesgt.Left = startX;
+                tesgt.BackColor = Color.Black;
+                tesgt.Name = "tesgt" + picBox_ID;
+
+                //Specifics for card
                 cardBack.Size = new Size(cardSizeX, cardSizeY);
                 cardBack.BackColor = Color.White;
-                cardBack.Left = startX;
-                cardBack.Top = startY;
-                cardBack.TabIndex = 5;
-                serverArea.Controls.Add(cardBack);
+                cardBack.Left = 0;
+                cardBack.Top = 0;
+                cardBack.Name = "cardBack" + picBox_ID;
+
+                //Ping
+                pingLabel.Size = new Size(cardSizeX, 30);
+                pingLabel.BackColor = Color.Red;
+                pingLabel.Location = new Point(0, 0);
+                pingLabel.Name = "pingLabel" + picBox_ID;
+
+                //Server name
+                serverNameLabel.Text = name;
+                serverNameLabel.Location = new Point(1, 5);
+                serverNameLabel.BackColor = Color.Red;
+                serverNameLabel.AutoSize = true;
+                serverNameLabel.Font = new System.Drawing.Font("Roboto", 12F);
+                serverNameLabel.ForeColor = Color.White;
+                serverNameLabel.Name = "serverNameLabel" + picBox_ID;
+
+                //Server IP
+                serverIPLabel.Text = "IP: " + ip;
+                serverIPLabel.Location = new Point(2, 40);
+                serverIPLabel.BackColor = Color.FromArgb(255, 255, 255);
+                serverIPLabel.Name = "serverIPLabel" + picBox_ID;
+
+                //Adding to form
+                serverArea.Controls.Add(tesgt);
+                tesgt.Controls.Add(serverNameLabel);
+                tesgt.Controls.Add(pingLabel);
+                tesgt.Controls.Add(serverIPLabel);
+                tesgt.Controls.Add(cardBack);
+
+                Console.WriteLine(picBox_ID.ToString());
+                //Ping
+                pingServer(ip, picBox_ID);
+
+                //Addding id
+                picBox_ID++;
+
+                //differences in pos
                 startX += cardBack.Width + 5;
                 if (startX > this.Width - cardSizeX)
                 {
                     startY += cardBack.Height + 5;
                     startX = 5;
                 }
-                
             }
         }
+        //Accept button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox1.Text == null && textBox1.Text == "" || textBox1.Text == null)
+            {
+                MessageBox.Show("Fill in the Name and IP fields.", "Error");
+            }
+            else
+            {
+                //Storing IP and Name
+                serverNames.Add(textBox1.Text);
+                serverIPs.Add(textBox2.Text);
+                serverCard(1, picBox_ID);
+            }
+
+        }
+
+        //Cancel button
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            textBox1.Visible = false;
+            textBox2.Visible = false;
+            button2.Visible = false;
+            button3.Visible = false;
+            pictureBox2.Visible = false;
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+
+        //ping
+        private void pingServer(string ip, int i)
+        {
+            bool pingable = false;
+            Ping pinger = new Ping();
+            try
+            {
+                PingReply reply = pinger.Send(ip);
+                pingable = reply.Status == IPStatus.Success;
+                
+                //Server is online, do stuff
+                if (pingable == true)
+                {
+                    Console.WriteLine(picBox_ID.ToString());
+                    Panel tempPanel = serverArea.Controls.OfType<Panel>().FirstOrDefault(x => x.Name == "tesgt" + i);
+                    PictureBox tempBack2 = tempPanel.Controls.OfType<PictureBox>().FirstOrDefault(x => x.Name == "pingLabel" + i);
+                    Label tempLabel2 = tempPanel.Controls.OfType<Label>().FirstOrDefault(x => x.Name == "serverNameLabel" + i);
+
+                    tempLabel2.BackColor = Color.Green;
+                    tempBack2.BackColor = Color.Green;
+
+                    //debug
+                    Console.WriteLine(ip + " is online.");
+
+                }
+            }
+            catch (Exception e)
+            {
+                //ignore
+                System.Windows.Forms.MessageBox.Show("Server is offline.", "Server Offline");
+
+            }
+        }
+
+        //Remove Card
+        private void removeCard(int i)
+        {
+            if (picBox_ID > 0)
+            {
+                //instance 
+                Panel tempPanel = serverArea.Controls.OfType<Panel>().FirstOrDefault(x => x.Name == "tesgt" + i);
+                PictureBox tempBack1 = tempPanel.Controls.OfType<PictureBox>().FirstOrDefault(x => x.Name == "cardBack" + i);
+                PictureBox tempBack2 = tempPanel.Controls.OfType<PictureBox>().FirstOrDefault(x => x.Name == "pingLabel" + i);
+                Label tempLabel1 = tempPanel.Controls.OfType<Label>().FirstOrDefault(x => x.Name == "serverIPLabel" + i);
+                Label tempLabel2 = tempPanel.Controls.OfType<Label>().FirstOrDefault(x => x.Name == "serverNameLabel" + i);
+
+                //Removes a serverCard
+                serverNames.RemoveAt(i);
+                serverIPs.RemoveAt(i);
+                tempPanel.Controls.Remove(tempBack1);
+                tempPanel.Controls.Remove(tempBack2);
+                tempPanel.Controls.Remove(tempLabel1);
+                tempPanel.Controls.Remove(tempLabel2);
+                serverArea.Controls.Remove(tempPanel);
+
+                //Changes name of items
+                for (int j = i + 1; j < picBox_ID; j++)
+                {
+                    Panel temp = serverArea.Controls.OfType<Panel>().FirstOrDefault(x => x.Name == "tesgt" + j);
+                    PictureBox temp1 = temp.Controls.OfType<PictureBox>().FirstOrDefault(x => x.Name == "cardBack" + j);
+                    PictureBox temp2 = temp.Controls.OfType<PictureBox>().FirstOrDefault(x => x.Name == "pingLabel" + j);
+                    Label temp3 = temp.Controls.OfType<Label>().FirstOrDefault(x => x.Name == "serverIPLabel" + j);
+                    Label temp4 = temp.Controls.OfType<Label>().FirstOrDefault(x => x.Name == "serverNameLabel" + j);
+
+                    temp.Name = "tesgt" + (j - 1);
+                    temp1.Name = "cardBack" + (j - 1);
+                    temp2.Name = "pingLabel" + (j - 1);
+                    temp3.Name = "serverIPLabel" + (j - 1);
+                    temp4.Name = "serverNameLabel" + (j - 1);
+
+                }
+                picBox_ID--;
+            }
+            else
+            {
+                MessageBox.Show("There are no servers to delete", "Error");
+            }
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //add popup for number to delete
+            removeCard(0);
+            Refresh();
+        }   
     }
 }
